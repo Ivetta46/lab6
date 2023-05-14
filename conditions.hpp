54a1de98ceeb0fbfb54f986007a00020f43d9896
Иветта Соловьева <soloveva.id@phystech.edu>
2023-05-14 23:25:47 +0300

    new

--- conditions.hpp
+++ conditions.hpp
@@ -9,6 +9,11 @@ double hx;
 double hy;
 double epsilon;
 
+double analitics(double x, double y, double t)
+{
+    return (1 + x + y) / std::pow(10 - 28 * t / 3.0, 2.0 / 3.0);
+}
+
 double left(double y, double t)
 {
     return std::pow(1 + y, 4.0 / 3.0) / std::pow(10 - 28 * t / 3.0, 2.0 / 3.0);
